416993e132ab4c1dc70ea909071ba00d0d0ef786
Hessam Imen <hessam.imen@gmail.com>
2022-05-21 08:31:19 +1000

    Done with assignmnet

---
+++ src/Messages.js
@@ -0,0 +1,10 @@
+// src/Messages.js
+import React from "react";
+
+class Messages extends React.Component {
+  render() {
+    return <h1>This is my Messages component!</h1>;
+  }
+}
+
+export default Messages;
